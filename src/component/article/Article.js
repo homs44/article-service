8e515886d189f3841aa229294b81f57d9f1dcd3d
ingyu yoon <a>
2019-06-30 11:13:49 +0900

    게시글 불러오기 추가

--- src/component/article/Article.js
+++ src/component/article/Article.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import ArticleItem from './ArticleItem';
 import { Loader } from 'semantic-ui-react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import * as articleActions from '../../module/article/actions'
 
 class Article extends Component {
 
@@ -9,17 +12,49 @@ class Article extends Component {
         article: null,
     }
 
+    componentDidMount() {
+        if (this.props.id) {
+            this.props.articleActions.getArticle(this.props.id);
+        }
+    }
+
     render() {
         const {
             article,
         } = this.props;
 
         return article ? (
-            <ArticleItem />
+            <ArticleItem
+                id={article.id}
+                downloadUrl={article.downloadUrl}
+                content={article.content}
+                commentCnt={article.commentCnt}
+                likeCnt={article.likeCnt}
+                createdAt={article.createdAt}
+                updatedAt={article.updatedAt}
+                userDisplayName={article.userDisplayName}
+                userId={article.userId}
+                userProfileUrl={article.userProfileUrl}
+                displayTimestamp={article.displayTimestamp}
+            />
         ) : (
-                <Loader active/>
+                <Loader active />
             )
     }
 }
 
-export default Article;
+const mapStateToProps = (state) => {
+    return {
+        isLoading: state.article.getArticle.isLoading,
+        error: state.article.getArticle.error,
+        article: state.article.getArticle.article
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        articleActions: bindActionCreators(articleActions, dispatch),
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Article);
